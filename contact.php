bfdbf7526d821f03fbd50c15eaded971a71b2276
student <student@aprotrain-aptech.edu>
2023-06-20 16:38:48 +0700

    ghep code

--- contact.php
+++ contact.php
@@ -1,37 +1,17 @@
 <?php
 include 'layouts/header.php';
-?>
-        <!-- main-search start -->
-        <div class="main-search-active">
-            <div class="sidebar-search-icon">
-                <button class="search-close"><i class="fa fa-times"></i></button>
-            </div>
-            <div class="sidebar-search-input">
-                <form action="#">
-                    <div class="form-search">
-                        <input id="search" class="input-text" value="" placeholder="" type="search">
-                        <button>
-                            <i class="fa fa-search"></i>
-                        </button>
-                    </div>
-                </form>
-                <p class="form-description">Hit enter to search or ESC to close</p>
-            </div>
-        </div>
-        <!-- main-search start -->
-
-
-        <!-- Page Banner Section Start -->
+?>  
+<!--Page Banner Section Start -->
+    <div>
         <div class="page-banner-section section bg-image" data-bg="assets/images/bg/breadcrumb.png">
             <div class="container">
                 <div class="row">
                     <div class="col">
-
                         <div class="page-banner text-start">
-                            <h2>Contact</h2>
+                            <h2>Contact Us</h2>
                             <ul class="page-breadcrumb">
                                 <li><a href="index.php">Home</a></li>
-                                <li>Contact</li>
+                                <li>Contact Us</li>
                             </ul>
                         </div>
 
@@ -39,64 +19,49 @@ include 'layouts/header.php';
                 </div>
             </div>
         </div>
-        <!-- Page Banner Section End -->
-        <!--Contact section start-->
-        <div class="conact-section section pt-85 pt-lg-65 pt-md-55 pt-sm-45 pt-xs-35  pb-100 pb-lg-80 pb-md-70 pb-sm-60 pb-xs-50">
-            <div class="container">
-
-                <div class="row">
-                    <div class="col-12">
-                        <div class="contact-form-wrap">
-                            <h3 class="contact-title">Write Us</h3>
-                            <form id="contact-form" action="https://htmldemo.net/nelson/nelson/assets/php/mail.php" method="post">
-                                <div class="row">
-                                    <div class="col-lg-5">
-                                        <div class="name-fild-padding mb-sm-30 mb-xs-30">
-                                            <div class="row">
-                                                <div class="col-md-12">
-                                                    <div class="contact-form-style mb-20">
-                                                        <label class="fild-name">Name</label>
-                                                        <input name="name" placeholder="" type="text">
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-12">
-                                                    <div class="contact-form-style mb-20">
-                                                        <label class="fild-name">Email</label>
-                                                        <input name="email" placeholder="" type="email">
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-12">
-                                                    <div class="check-box">
-                                                        <input type="checkbox" id="create_account">
-                                                        <label for="create_account">I am not a robot</label>
-                                                    </div>
-                                                </div>
+    </div>
+    <!-- Page Banner Section End -->
+    <!--Contact section start-->
+    <div class="conact-section section pt-85 pt-lg-65 pt-md-55 pt-sm-45 pt-xs-35  pb-100 pb-lg-80 pb-md-70 pb-sm-60 pb-xs-50">
+        <div class="container" >
+            <div class="row">
+                <div class="col-12">
+                    <div class="contact-form-wrap">
+                        <h3 class="contact-title">Contact Us</h3>
+                            <div class="row align-items-center">
+                                <div class="col-lg-6 col-12">
+                                <!--Contact Us Content Start-->
+                                    <div class=" pl-30 pl-md-0 pl-sm-0 pl-xs-0">
+                                        <div class="contact-information">
+                                            <h4>Address:</h4>
+                                            <p>APTECH building, 285 P. Doi Can, Lieu Giai, Ba Dinh, Hanoi.</p>
+                                            <h4>Phone number:</h4>
+                                            <p>094372876</p>
+                                            <h4>Gmail:</h4>
+                                            <p><a href="https://mail.google.com/mail/u/a@gmail.com/#inbox" target="_blank">a@gmail.com</a></p>
+                                        </div>
+                                    </div>    
+                                </div>
+                                <!--Contact Us Content End-->
+                                <!--Contact Map section start-->
+                                <div class="col-lg-6 col-12">
+                                    <div class="contact-map-section section">
+                                        <div id="contact-map" >
+                                            <div class = "row align-items-center">
+                                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14895.706232288587!2d105.80092615541992!3d21.035624400000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab0d6e603741%3A0x208a848932ac2109!2sAptech%20Computer%20Education!5e0!3m2!1sen!2s!4v1687194864543!5m2!1sen!2s" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-lg-7">
-                                        <div class="contact-form-style bl">
-                                            <label class="fild-name pl-15">Message</label>
-                                            <textarea class="pl-15" name="message" placeholder="Type your message here.."></textarea>
-                                            <button class="btn" type="submit"><span>Send message</span></button>
-                                        </div>
-                                    </div>
                                 </div>
-                            </form>
-                            <p class="form-messege"></p>
-                        </div>
+                                <!--Contact Map section End-->
+                            </div>
                     </div>
                 </div>
-
             </div>
         </div>
-        <!--Contact section end-->
-        <!--Contact Map section start-->
-        <div class="contact-map-section section">
-            <div id="contact-map" class="contact-map"></div>
-        </div>
-        <!--Contact Map section End-->
-       
-        <?php
+    </div>
+</div>
+<!-- Contact Section End -->
+<?php
 include 'layouts/footer.php';
 ?>
